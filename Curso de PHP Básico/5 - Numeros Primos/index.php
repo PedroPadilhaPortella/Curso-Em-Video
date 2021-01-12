1501b5da7304e3347100309fe5d9c42601c3bae2
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2021-01-12 12:35:31 -0300

    correction

--- Curso de PHP Básico/5 - Numeros Primos/index.php
+++ Curso de PHP Básico/5 - Numeros Primos/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,8 +9,8 @@
     <h1>Verificador de Numeros Primos</h1>
     <p>Insira um valor numerico para ser verificado</p>
     <form action="Numeroprimo.php" method="get">
-    <input type="number" name="valor" value="valor">
-    <input type="submit" value="calcular">
+        <input type="number" name="valor" value="valor">
+        <input type="submit" value="calcular">
     </form>
 </body>
 </html>
